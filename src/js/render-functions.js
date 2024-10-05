d0034a3f924e04e43c0025917b443e92c4b1e4e0
sashko-kapillar <kapillarua@gmail.com>
2024-10-05 21:52:07 +0300

    delete limit 15

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -5,7 +5,6 @@ const loader = document.querySelector('.loader');
 export const apiKey = '46100469-a9a71a6d23d5188e64cb63582';
 
 export const searchParams = new URLSearchParams({
-  _limit: 15,
   image_type: 'photo',
   orientation: 'horizontal',
   safesearch: true,
